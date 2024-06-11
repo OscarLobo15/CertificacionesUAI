35a9df2adba2cf6a3869dd11c65d5ffdd3d897f1
Jose Chamorro <jmchamorr@outlook.com>
2024-06-11 16:33:25 -0400

    DASHBOARD FINALIZADO

--- src/i18n.js
+++ src/i18n.js
@@ -1,23 +1,20 @@
+// src/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enTranslation from './JSON/en.json';
-import esTranslation from './/JSON/es.json';
+import en from './JSON/en.json';
+import es from './JSON/es.json';
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
-      en: {
-        translation: enTranslation,
-      },
-      es: {
-        translation: esTranslation,
-      },
+      en: { translation: en },
+      es: { translation: es },
     },
-    lng: 'es', // idioma predeterminado
-    fallbackLng: 'es', // idioma de respaldo si la traducción no está disponible
+    lng: 'es', // default language
+    fallbackLng: 'es',
     interpolation: {
-      escapeValue: false, // No necesitas escapar HTML en las traducciones
+      escapeValue: false, // react already safes from xss
     },
   });
 
